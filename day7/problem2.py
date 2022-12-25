b7a2cc5fb83d8df050a2191110a2a3676f123572
Alex <78465604+alex-clinton@users.noreply.github.com>
2022-12-24 22:38:23 -0500

    Day 7 Problem 2

---
+++ day7/problem2.py
@@ -0,0 +1,24 @@
+import sys
+import os
+
+def main():
+    sizes = {}
+    curr_path = '/'
+
+    for line in sys.stdin:
+        # Step out of current directory
+        if 'cd ..' in line:
+            curr_path, _ = os.path.split(curr_path)
+        # Step into new directory
+        elif ' cd ' in line:
+            curr_path = os.path.join(curr_path, line.split()[2])
+            sizes[curr_path] = 0
+        # Add to directories that the current file is within
+        elif (size := line.split()[0]).isnumeric():
+            for path in filter(lambda x: x in curr_path, sizes.keys()):
+                sizes[path] += int(size)
+
+    print(min(filter(lambda x: x >= sizes['/']-40000000, sizes.values())))
+
+if __name__ == '__main__':
+    main()
